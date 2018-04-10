2c3e39c35cdbad80f6c0f01d0d20ec44e8d61a6a
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-04-10 15:51:22 +0100

    Add ch client

--- company/utils.py
+++ company/utils.py
@@ -1,9 +1,15 @@
 import csv
+import http
 import io
+import urllib
 import zipfile
+from collections import OrderedDict
 from contextlib import contextmanager
+from functools import partial
+from urllib.parse import urljoin
 
 import requests
+from django.conf import settings
 
 from company.doctypes import CompanyDocType
 
@@ -66,6 +72,8 @@ COMPANY_TYPES = {
     'Protected Cell Company': 'protected-cell-company',
     'Investment Company with Variable Capital (Securities)': 'icvc-securities',
 }
+
+MESSAGE_AUTH_FAILED = 'Auth failed with Companies House'
 
 
 @contextmanager
@@ -133,3 +141,68 @@ def create_company_document(row):
         meta={'id': company['company_number']},
         **company
     )
+
+
+class CompaniesHouseClient:
+    api_key = settings.COMPANIES_HOUSE_API_KEY
+    client_id = settings.COMPANIES_HOUSE_CLIENT_ID
+    client_secret = settings.COMPANIES_HOUSE_CLIENT_SECRET
+    make_api_url = partial(urljoin, 'https://api.companieshouse.gov.uk')
+    make_oauth2_url = partial(urljoin, 'https://account.companieshouse.gov.uk')
+    endpoints = {
+        'profile': make_api_url('company/{number}'),
+        'address': make_api_url('company/{number}/registered-office-address'),
+        'search': make_api_url('search/companies'),
+        'oauth2': make_oauth2_url('oauth2/authorise'),
+        'oauth2-token': make_oauth2_url('oauth2/token'),
+    }
+    session = requests.Session()
+
+    @classmethod
+    def get_auth(cls):
+        return requests.auth.HTTPBasicAuth(cls.api_key, '')
+
+    @classmethod
+    def get(cls, url, params={}):
+        response = cls.session.get(url=url, params=params, auth=cls.get_auth())
+        if response.status_code == http.client.UNAUTHORIZED:
+            logger.error(MESSAGE_AUTH_FAILED)
+        return response
+
+    @classmethod
+    def retrieve_profile(cls, number):
+        url = cls.endpoints['profile'].format(number=number)
+        return cls.get(url)
+
+    @classmethod
+    def retrieve_address(cls, number):
+        url = cls.endpoints['address'].format(number=number)
+        return cls.get(url)
+
+    @classmethod
+    def search(cls, term):
+        url = cls.endpoints['search']
+        return cls.get(url, params={'q': term})
+
+    @classmethod
+    def make_oauth2_url(cls, redirect_uri, company_number):
+        # ordered dict to facilitate testing
+        params = OrderedDict([
+            ('client_id', cls.client_id),
+            ('redirect_uri', redirect_uri),
+            ('response_type', 'code'),
+            ('scope', cls.endpoints['profile'].format(number=company_number)),
+        ])
+        return cls.endpoints['oauth2'] + '?' + urllib.parse.urlencode(params)
+
+    @classmethod
+    def verify_oauth2_code(cls, code, redirect_uri):
+        url = cls.endpoints['oauth2-token']
+        params = OrderedDict([
+            ('grant_type', 'authorization_code'),
+            ('code', code),
+            ('client_id', cls.client_id),
+            ('client_secret', cls.client_secret),
+            ('redirect_uri', redirect_uri),
+        ])
+        return cls.session.post(url=url + '?' + urllib.parse.urlencode(params))
