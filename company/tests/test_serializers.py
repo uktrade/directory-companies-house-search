7b6d63f4bbef1736038b11f3eb48a64e5c9aac64
richtier <rikatee@gmail.com>
2019-01-31 13:57:42 +0000

    Fix serialization of companies house data

--- company/tests/test_serializers.py
+++ company/tests/test_serializers.py
@@ -1,8 +1,18 @@
 from company import serializers
 
 
+def test_company_serializer_null_address():
+    serializer = serializers.CompanySearchResultSerializer(data={})
+
+    serializer.is_valid()
+
+    assert 'address' not in serializer.errors
+
+
 def test_company_serializer_no_address():
-    serializer = serializers.CompanySerializer(data={'address': None})
+    serializer = serializers.CompanySearchResultSerializer(
+        data={'address': None}
+    )
 
     serializer.is_valid()
 
@@ -11,6 +21,14 @@ def test_company_serializer_no_address():
 
 def test_company_serializer_no_date_of_creation():
     serializer = serializers.CompanySerializer(data={})
+
+    serializer.is_valid()
+
+    assert 'date_of_creation' not in serializer.errors
+
+
+def test_company_serializer_null_date_of_creation():
+    serializer = serializers.CompanySerializer(data={'date_of_creation': None})
 
     serializer.is_valid()
 
